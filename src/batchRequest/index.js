bcd77f19a7f253b650c6afff3d1b6226fe6285e7
Pradeep Bande <pradeep@expand.network>
2024-04-25 16:05:23 +0530

    added nonce to signTransaction

--- src/batchRequest/index.js
+++ src/batchRequest/index.js
@@ -5,16 +5,20 @@ const SEPOLIA_RPC_URL = config?.rpc_url?.sepolia
 
 // Connect to Ethereum network
 const web3 = new Web3(SEPOLIA_RPC_URL);
+async function getNonce(account) {
+  return await web3.eth.getTransactionCount(account, 'pending');
+}
 
 module.exports = {
-  batchRequest: async (transactionObjects, privateKey) => {
+  batchRequest: async (transactionObjects, account, privateKey) => {
     if (transactionObjects.length > 0) {
       let count = 0;
+      let nonce = await getNonce(account);
       const batch = new web3.BatchRequest();
-      while (transactionObjects.length > count) { 
-        const signedTx = await web3.eth.accounts.signTransaction(transactionObjects[count], privateKey);
+      while (transactionObjects.length > count) {
+        const signedTx = await web3.eth.accounts.signTransaction({ ...transactionObjects[count], nonce: nonce + count }, privateKey);
         await new Promise((resolve, reject) => {
-          batch.add(web3.eth.sendSignedTransaction(signedTx.rawTransaction, (err, data) => { 
+          batch.add(web3.eth.sendSignedTransaction(signedTx.rawTransaction, (err, data) => {
             if (err) {
               console.error('Error executing approve transaction:', err);
               reject(err);
