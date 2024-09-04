f55ce4bffa20730304630e52a8b5af4b83b667a6
Tek Raj Joshi <tekrajjoshi07@gmail.com>
2024-09-04 16:10:13 +0530

    signer implementation updated for token transfer

--- test/stacksTest.js
+++ test/stacksTest.js
@@ -1,45 +1,25 @@
 const { WalletStacks } = require('../src/index');
 const dotenv = require('dotenv');
-const {
-  AnchorMode,
-  makeUnsignedSTXTokenTransfer,
-  createStacksPrivateKey,
-  getPublicKey,
-  publicKeyToString,
-} = require("@stacks/transactions");
-
 
 const main = async () => {
   dotenv.config();
 
-  const privateKey = process.env.xrplSecretKey;
-  const recipient = "<wallet address>"; 
+  const privateKey = process.env.stacksPrivateKey;
+  const recipient = "<recipient>"; 
 
   // Initialise the wallet client
   const wallet = new WalletStacks({ xApiKey: process.env.xApiKey, privateKey });
 
-  const privateKeyBuffer = createStacksPrivateKey(privateKey);
-  const publicKeyBuffer = getPublicKey(privateKeyBuffer);
-  const publicKey = publicKeyToString(publicKeyBuffer);
-
-  // Transfer token function from Stacks SDK
-  const transaction = await makeUnsignedSTXTokenTransfer({
-    network: "testnet",
-    recipient,
-    amount: "100",
-    fee: "300",
-    memo: "Expand",
-    publicKey: publicKey,
-    anchorMode: AnchorMode.Any,
-  });
-
-  const preparedTx = {
+  const preparedTx =  {
     chainId: "1701",
-    data: transaction
+    to: recipient,
+    value: "3000",
+    message: "Expand STACKS Test"
   }
 
   // Securely sign the transaction on user's end
   const signedTx = await wallet.signTransaction(preparedTx);
+  console.log(signedTx)
   const tx = await wallet.sendTransaction(signedTx);
 
   console.log("Tx: ", tx);
