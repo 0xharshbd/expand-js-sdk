2ff4b0fbd0cfce646d7670596061bd05700aec54
Tek Raj Joshi <tekrajjoshi07@gmail.com>
2024-09-27 11:44:33 +0530

    Bitcoin getkeys function made static

--- src/adapters/WalletBitcoin/index.js
+++ src/adapters/WalletBitcoin/index.js
@@ -46,15 +46,15 @@ class WalletBitcoin {
     // To Do: get all the UTXOs
     txb.addInput(utxo.txId, utxo.vout);
     txb.addOutput(to, Number(value));
-    
+
     const fee = config.chains[chainId].fee;
     const actualAmount = utxo.value - Number(value) - fee;
 
     if (actualAmount > 0) {
       txb.addOutput(from, actualAmount);
     }
-    
-    txb.sign(0, keyPair);    
+
+    txb.sign(0, keyPair);
     const rawTx = txb.build().toHex();
     return { chainId, rawTransaction: rawTx };
   };
@@ -63,7 +63,7 @@ class WalletBitcoin {
     const filterOptions = options;
     filterOptions.function = "sendTransaction()";
     const validJson = await schemaValidator.validateInput(filterOptions);
-    
+
     if (!validJson.valid) {
       return (validJson);
     }
@@ -87,54 +87,54 @@ class WalletBitcoin {
       return error;
     }
   };
+};
 
-  getKeysFromMnemonic = async (options, path = "m/44'/0'/0'/0/0") => {
-    options.function = "BTCGetKeys()";
-    const validJson = await schemaValidator.validateInput(options);
+const getKeysFromMnemonic = async (options, path = "m/44'/0'/0'/0/0") => {
+  options.function = "BTCGetKeys()";
+  const validJson = await schemaValidator.validateInput(options);
 
-    if (!validJson.valid) {
-      return validJson;
-    }
-
-    let { chainId } = options;
-    const { chainSymbol, mnemonic } = options;
-
-    chainId = await common.getChainId({ chainId, chainSymbol });
-    const chainName = config.chains[chainId]?.chainName;
-
-    if (chainName !== "Bitcoin") {
-      return {
-        "msg": "Bitcoin wallet can be used only with Bitcoin Wallet"
-      }
-    };
-
-    const seed = await bip39.mnemonicToSeed(mnemonic);
-    const network = chainId === "1800" ? bitcoin.networks.mainnet : bitcoin.networks.testnet;
-    
-    // Create an HD wallet from the seed
-    const root = bitcoin.bip32.fromSeed(seed, network);
-
-    // Derive the key at the specified path
-    const child = root.derivePath(path);
-    
-    // Get the private key in WIF (Wallet Import Format)
-    const privateKeyWIF = child.toWIF();
-
-    // Decode WIF to get the actual private key in hexadecimal
-    const privateKeyBuffer = bitcoin.ECPair.fromWIF(child.toWIF(), network).privateKey;
-    const privateKeyHex = privateKeyBuffer.toString('hex');
-
-    // Get the public Key
-    const publickKey = child.publicKey.toString('hex');
-
-    // Get the public address (Pay to pubkey hash)
-    const { address } = bitcoin.payments.p2pkh({
-        pubkey: child.publicKey,
-        network: network
-    });
-    
-    return { privateKeyWIF, privateKeyHex, publickKey, address };
+  if (!validJson.valid) {
+    return validJson;
   }
+
+  let { chainId } = options;
+  const { chainSymbol, mnemonic } = options;
+
+  chainId = await common.getChainId({ chainId, chainSymbol });
+  const chainName = config.chains[chainId]?.chainName;
+
+  if (chainName !== "Bitcoin") {
+    return {
+      "msg": "Bitcoin wallet can be used only with Bitcoin Wallet"
+    }
+  };
+
+  const seed = await bip39.mnemonicToSeed(mnemonic);
+  const network = chainId === "1800" ? bitcoin.networks.mainnet : bitcoin.networks.testnet;
+
+  // Create an HD wallet from the seed
+  const root = bitcoin.bip32.fromSeed(seed, network);
+
+  // Derive the key at the specified path
+  const child = root.derivePath(path);
+
+  // Get the private key in WIF (Wallet Import Format)
+  const privateKeyWIF = child.toWIF();
+
+  // Decode WIF to get the actual private key in hexadecimal
+  const privateKeyBuffer = bitcoin.ECPair.fromWIF(child.toWIF(), network).privateKey;
+  const privateKeyHex = privateKeyBuffer.toString('hex');
+
+  // Get the public Key
+  const publickKey = child.publicKey.toString('hex');
+
+  // Get the public address (Pay to pubkey hash)
+  const { address } = bitcoin.payments.p2pkh({
+    pubkey: child.publicKey,
+    network: network
+  });
+
+  return { privateKeyWIF, privateKeyHex, publickKey, address };
 }
 
-module.exports = { WalletBitcoin }; 
+module.exports = { WalletBitcoin, getKeysFromMnemonic }; 
