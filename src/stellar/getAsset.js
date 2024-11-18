ec7f8ddfd026635d513ac96b41d9570cfb3e8e28
saqib-cmlab <saqib@ix.cumberlandlabs.io>
2024-11-18 18:06:05 +0530

    removed comments

--- src/stellar/getAsset.js
+++ src/stellar/getAsset.js
@@ -7,33 +7,6 @@ const schemaValidator = require('../../configuration/schemaValidator');
 const { bufferToString, getTransactionByHash } = require('./helpers');
 // const errorMessage = require('../../configuration/errorMessage.json');
 
-// function bufferToString(buffer) {
-//     return buffer.toString('utf-8');
-//   }
-
-// async function getTransactionByHash(chainId,transactionHash) {
-//     const requestBody = {
-//       "jsonrpc": "2.0",
-//       "id": 8675309,
-//       "method": "getTransaction",
-//       "params": {
-//         "hash": transactionHash
-//       }
-//     };
-  
-//     try {
-//       const res = await axios.post(config.chains[chainId].sorobanRpc, requestBody, {
-//         headers: {
-//           'Content-Type': 'application/json',
-//         }
-//       });
-//       return res.data;
-//     } catch (error) {
-//       console.error('Error making request:', error);
-//       throw error;
-//     }
-//   }
-
 module.exports = {
     getAsset: async (options) => {
         const filterOptions = options;
